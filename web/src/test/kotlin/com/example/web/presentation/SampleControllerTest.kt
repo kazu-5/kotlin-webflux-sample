220857e13aa6c5a0ad597ef2124244e8c8d42712
吉本和弘 <kazuy.dj9@gmail.com>
2023-01-02 10:36:15 +0900

    commit

--- web/src/test/kotlin/com/example/web/presentation/SampleControllerTest.kt
+++ web/src/test/kotlin/com/example/web/presentation/SampleControllerTest.kt
@@ -3,13 +3,15 @@ package com.example.web.presentation
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest
-@AutoConfigureWebTestClient(timeout = "PT10S")
-class SampleControllerTest(val client: WebTestClient) {
+@AutoConfigureWebTestClient
+class SampleControllerTest @Autowired constructor(val client: WebTestClient) {
 
     @Test
     fun index() {
@@ -18,7 +20,5 @@ class SampleControllerTest(val client: WebTestClient) {
             .uri("/test")
             .exchange()
             .expectStatus().isOk
-            .expectBody()
-            .json("{\"name\":\"Jane\"}")
     }
 }
